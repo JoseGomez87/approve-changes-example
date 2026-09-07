10559c038f20bf477a66460868fa3ac3299679b5
Garlang87 <jaime_sg87@hotmail.com>
2026-09-06 20:18:28 -0600

    Carga inicial

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Prints "Hello, World!" and moves the cursor to a new line
+        System.out.println("Hello, World!"); 
+        
+        int age = 25;
+        // You can print variables and combine them with text using '+'
+        System.out.println("Age: " + age); 
+    }
+}
